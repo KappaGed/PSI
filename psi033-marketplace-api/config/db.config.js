ea4d7c829f71b7d36223c39f0e5cd52ee833c1b7
marina-del-rey <marski00@protonmail.com>
2023-04-26 03:56:44 +0100

    Fixed API not connecting to Mongodb db

--- psi033-marketplace-api/config/db.config.js
+++ psi033-marketplace-api/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://marketplace_user:&Rfq7AY^TpfVnU@localhost:27017/marketplace"
-  }
+  // this is the name of my local mongo db, pls change it accordingly
+  url: "mongodb://marketplace_user:&Rfq7AY^TpfVnU@127.0.0.1:27017/marketplace?authSource=marketplace"
+}
